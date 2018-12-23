7b24d61b988d4dbcca3ff4496769d7f62789b19f
TakTak123 <TakTak123@users.noreply.github.com>
2018-12-23 20:32:47 +0900

    変数等の名前を修正

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,42 @@
 const boxCount = 10;
-let mainColorRGB = new Array(3);
-
+let main_color_rgb = new Array(3);
 
 function changeMainColor() { //問題の色を変更．
   "use strict";
 
-  let mainColorBox = document.getElementById("mainBox");
+  let main_color_box = document.getElementById("main_box");
 
   for (let i = 0; i < 3; i++) {
-    mainColorRGB[i] = Math.floor(Math.random() * 256);
+    main_color_rgb[i] = Math.floor(Math.random() * 256);
   }
-  let text = "RGB(" + mainColorRGB + ")";
-  mainColorBox.style.backgroundColor = "rgb(" + mainColorRGB + ")"; 
-  mainColorBox.dataset.color = mainColorRGB; //datasetに色情報を追加．
+  let text = "RGB(" + main_color_rgb + ")";
+  main_color_box.style.backgroundColor = "rgb(" + main_color_rgb + ")"; 
+  main_color_box.dataset.color = main_color_rgb; //datasetに色情報を追加．
 
-  document.getElementById("mainColorText").innerText = text;
+  document.getElementById("main_color_text").innerText = text;
 }
 
 function changeColor(id) { //選択肢の色を変更．基本的にchangeMainColor()と同じだが，色はmainColorを基準にわずかに変更している．
   "use strict";
 
-  let colorBox = document.getElementById(id);
-  let colorRGB = new Array(3);
-  let textColorRGB;
+  let color_box = document.getElementById(id);
+  let color_rgb = new Array(3);
+  let color_rgb_text;
 
   for (let i = 0; i < 3; i++) {
-    colorRGB[i] = mainColorRGB[i] + Math.floor(Math.random() * 2 + 10);
-    if (colorRGB[i] > 255) {
-      colorRGB[i] = 255;
+    color_rgb[i] = main_color_rgb[i] + Math.floor(Math.random() * 2 + 10);
+    if (color_rgb[i] > 255) {
+      color_rgb[i] = 255;
     }
   }
   if (("box" + Math.ceil(Math.random() * boxCount)) === id) { //1つだけ問題と同じ色に設定
     for (let i = 0; i < 3; i++) {
-      colorRGB[i] = mainColorRGB[i]
+      color_rgb[i] = main_color_rgb[i]
     }
   }
-  textColorRGB = "RGB(" + colorRGB + ")";
-  colorBox.style.backgroundColor = "rgb(" + colorRGB + ")"; 
-  colorBox.dataset.color = colorRGB;
+  color_rgb_text = "RGB(" + color_rgb + ")";
+  color_box.style.backgroundColor = "rgb(" + color_rgb + ")"; 
+  color_box.dataset.color = color_rgb;
 
 }
 
@@ -47,21 +46,21 @@ function createColorBox() {
   let container = document.getElementsByClassName("container");
 
   for (let i = 0; i < boxCount; i++) {
-    let colorBox = document.createElement("div");
-    colorBox.classList.add("box");
-    colorBox.setAttribute("id", "box" + String(i));
-    container[0].appendChild(colorBox); //getElementByClassName()は配列が返ってくる．
-    // document.body.appendChild(colorBox);
-    colorBox.addEventListener("click", function() { //正解か判定
-      let mainColorBox = document.getElementById("mainBox");
-      if (this.dataset.color === mainColorBox.dataset.color) {
+    let color_box = document.createElement("div");
+    color_box.classList.add("box");
+    color_box.setAttribute("id", "box" + String(i));
+    container[0].appendChild(color_box); //getElementByClassName()は配列が返ってくる．
+    // document.body.appendChild(color_box);
+    color_box.addEventListener("click", function() { //正解か判定
+      let main_color_box = document.getElementById("main_box");
+      if (this.dataset.color === main_color_box.dataset.color) {
         document.getElementById("judge").innerText = "Correct!"
       }
       else {
         document.getElementById("judge").innerText = "Not Correct..."
       }
     }, false) 
-    changeColor(colorBox.id);
+    changeColor(color_box.id);
   }
 }
 
